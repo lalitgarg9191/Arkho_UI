bb56c0b123a090b35c0c5ac687aac0c6d5991cf5
gouravfreedom <gouravfreedom@gmail.com>
2019-08-05 22:34:15 +0530

    payment changes

--- DFS/Views/PaymentPage.xaml.cs
+++ DFS/Views/PaymentPage.xaml.cs
@@ -16,18 +16,21 @@ namespace DFS.Views
 
         async void WebView_Navigated(object sender, WebNavigatedEventArgs e)
         {
-            if (e.Url.StartsWith("http://104.238.81.169:4080",StringComparison.CurrentCultureIgnoreCase)) {
-                var arr = e.Url.Split('?');
-                var paymentData = arr[1];
-                var arr1 = paymentData.Split('=');
-                var paymentId = arr1[1].Split('&')[0];
-                var tokenId = arr1[2].Split('&')[0];
-                if (!string.IsNullOrEmpty(paymentId)) {
-                    await DisplayAlert("Alert", "Payment successfull!", "OK");
-                    App.Current.MainPage = new RootPage(App.SelectedView);
-                }
+            if (e.Url.StartsWith("http://104.238.81.169:4080/FitnessApp/manageservices/v1/members/process/payment", StringComparison.CurrentCultureIgnoreCase))
+            {
+                await DisplayAlert("Alert", "Payment successfull!", "OK");
+                App.Current.MainPage = new RootPage(App.SelectedView);
+
+            }
+
+            if (e.Url.StartsWith("http://104.238.81.169:4080/FitnessApp/manageservices/v1/members/cancel/payment", StringComparison.CurrentCultureIgnoreCase))
+            {
+                await DisplayAlert("Alert", "Payment Failure!", "OK");
+                await this.Navigation.PopAsync();
+
             }
         }
+
 
     }
 }
