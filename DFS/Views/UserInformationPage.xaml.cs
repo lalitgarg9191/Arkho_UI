3727c66d718042e5f88bd9ef705c2c33f76d45f0
gouravfreedom <gouravfreedom@gmail.com>
2019-09-23 00:58:37 +0530

    bug fixes

--- DFS/Views/UserInformationPage.xaml.cs
+++ DFS/Views/UserInformationPage.xaml.cs
@@ -40,7 +40,7 @@ namespace DFS.Views
             MessagingCenter.Subscribe<ViewModels.SignupViewModel>(this, "SignUpSuccess", (sender) =>
             {
                 var member = App.SelectedView == "Trainee" ? App.LoginResponse : App.TrainerData;
-                CredentialsService.SaveCredentials(signupViewModel.EmailAddress, signupViewModel.Password, member, App.FacebookUser, userType: App.SelectedView);
+                CredentialsService.SaveCredentials(signupViewModel.EmailAddress, signupViewModel.Password, App.LoginResponse, App.FacebookUser, userType: App.SelectedView);
 
                 if (App.TrainerStripeUrl != "")
                 {
